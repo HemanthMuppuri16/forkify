b3606d21e611b6fb8f1445689ed2e2652fb1b0e5
HemanthMuppuri16 <muppurihemanth16@gmail.com>
2023-06-21 16:09:11 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '29aa4e1b-1860-45d2-ac8a-578aa2b07d73';
+export const MODAL_CLOSE_SEC = 2.5;
