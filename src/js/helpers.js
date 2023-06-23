4d2459c2c1e24b9af150c3a9f3298ba5534f8c41
HemanthMuppuri16 <muppurihemanth16@gmail.com>
2023-06-22 20:50:29 -0500

    retest

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -30,8 +30,8 @@ export const AJAX = async function (url, uploadData = undefined) {
     throw err;
   }
 };
-/*
-export const numberToFraction = function (amount) {
+
+export const Fraction = function (amount) {
   // This is a whole number and doesn't need modification.
   if (parseFloat(amount) === parseInt(amount)) {
     return amount;
@@ -63,7 +63,7 @@ export const numberToFraction = function (amount) {
   return amount;
 };
 
-
+/*
 export const getJSON = async function (url) {
   try {
     const fetchPro = fetch(url);
