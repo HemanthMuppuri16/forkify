4e2916f6fce89ad708caa21b2673cb42a41b94ac
HemanthMuppuri16 <muppurihemanth16@gmail.com>
2023-06-22 20:29:56 -0500

    testing

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -128,9 +128,9 @@ class RecipeView extends View {
       <svg class="recipe__icon">
         <use href="${icons}#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${
-        ing.quantity ? new numberToFraction(ing.quantity).toString() : ''
-      }</div>
+       <div class="recipe__quantity">${
+         ing.quantity ? new numberToFraction(ing.quantity) : ''
+       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
         ${ing.description}
